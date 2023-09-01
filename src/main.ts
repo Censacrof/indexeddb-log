ae4425d0284cfc50b478e733c77bf528fc04bad8
Francesco Galisi <francesco.galisi@imagicle.com>
2023-09-01 13:12:37 +0200

    Add log objectStore

--- src/main.ts
+++ src/main.ts
@@ -2,70 +2,42 @@ import { unwrap } from "./unwrap";
 
 export { };
 
-const request = window.indexedDB.open("MyTestDatabase", 4);
+const request = window.indexedDB.open("MyTestDatabase", 1);
 
 request.onupgradeneeded = async (event) => {
   console.log("upgrade needed");
   const db = (event.target as IDBOpenDBRequest).result;
   console.log({event})
 
-  const tokensStore = db.createObjectStore("tokens", {
-    keyPath: "token",
-  });
+  // const tokensStore = db.createObjectStore("tokens", {
+  //   keyPath: "token",
+  // });
+  // await unwrap(tokensStore.transaction);
 
-  const transaction: IDBTransaction = tokensStore.transaction;
-  await unwrap(transaction);
+  const logStore = db.createObjectStore("logs", {
+    keyPath: "id",
+    autoIncrement: true
+  });
+  
+  logStore.createIndex("date", "date");
+
+  await unwrap(logStore.transaction);
 };
 
 const db = await unwrap(request);
 
-const transaction = db.transaction(["tokens"], "readwrite");
+const log = async (message: string) => {
+  const transaction = db.transaction(["logs"], "readwrite");
+  const logStore = transaction.objectStore("logs");
 
-const tokensStore = transaction.objectStore("tokens");
+  await unwrap(logStore.add({
+    date: new Date(),
+    message
+  }))
 
-const res = await unwrap(tokensStore.add({
-  token: "gigiiii",
-  asdasdasd: "fuffo"
-}));
-
-console.log({res})
-
+  console.log(message)
+}
 
 
-// const dbName = "the_name";
+await log("ciao!!");
 
-// const request = indexedDB.open("boiadeh", 2);
-
-// // request.onerror = (event) => {
-// //   Handle errors.
-// // };
-
-// request.onupgradeneeded = (event) => {
-//   console.log("cane");
-//   const db = event.target.result;
-
-//   // Create an objectStore to hold information about our customers. We're
-//   // going to use "ssn" as our key path because it's guaranteed to be
-//   // unique - or at least that's what I was told during the kickoff meeting.
-//   const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
-
-//   // Create an index to search customers by name. We may have duplicates
-//   // so we can't use a unique index.
-//   objectStore.createIndex("name", "name", { unique: false });
-
-//   // Create an index to search customers by email. We want to ensure that
-//   // no two customers have the same email, so use a unique index.
-//   objectStore.createIndex("email", "email", { unique: true });
-
-//   // Use transaction oncomplete to make sure the objectStore creation is
-//   // finished before adding data into it.
-//   objectStore.transaction.oncomplete = (event) => {
-//     // Store values in the newly created objectStore.
-//     const customerObjectStore = db
-//       .transaction("customers", "readwrite")
-//       .objectStore("customers");
-//     customerData.forEach((customer) => {
-//       customerObjectStore.add(customer);
-//     });
-//   };
-// };
