23eb6b077fec8acc520b816b0628b7001fcd900f
Francesco Galisi <galisifrancesco@gmail.com>
2023-09-01 12:25:30 +0200

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,57 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { unwrap } from "./unwrap";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+export {};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const request = window.indexedDB.open("MyTestDatabase", 3);
+
+request.onupgradeneeded = async (event) => {
+  console.log("upgrade needed");
+  // const db = event.target?.addEventListener
+
+  // const tokensStore = db.createObjectStore("tokens", {
+  //   keyPath: "token",
+  // });
+
+  // const transaction: IDBTransaction = tokensStore.transaction;
+  // await unwrap(transaction);
+};
+
+const db = await unwrap(request);
+console.log(db);
+
+// const dbName = "the_name";
+
+// const request = indexedDB.open("boiadeh", 2);
+
+// request.onerror = (event) => {
+//   Handle errors.
+// };
+// request.onupgradeneeded = (event) => {
+//   console.log("cane");
+//   const db = event.target.result;
+
+//   // Create an objectStore to hold information about our customers. We're
+//   // going to use "ssn" as our key path because it's guaranteed to be
+//   // unique - or at least that's what I was told during the kickoff meeting.
+//   const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+//   // Create an index to search customers by name. We may have duplicates
+//   // so we can't use a unique index.
+//   objectStore.createIndex("name", "name", { unique: false });
+
+//   // Create an index to search customers by email. We want to ensure that
+//   // no two customers have the same email, so use a unique index.
+//   objectStore.createIndex("email", "email", { unique: true });
+
+//   // Use transaction oncomplete to make sure the objectStore creation is
+//   // finished before adding data into it.
+//   objectStore.transaction.oncomplete = (event) => {
+//     // Store values in the newly created objectStore.
+//     const customerObjectStore = db
+//       .transaction("customers", "readwrite")
+//       .objectStore("customers");
+//     customerData.forEach((customer) => {
+//       customerObjectStore.add(customer);
+//     });
+//   };
+// };
